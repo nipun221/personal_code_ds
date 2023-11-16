9db75c0786d730fd7f5046d2b90635582d1add7c
nipun221 <2oics072@gbu.ac.in>
2023-11-17 00:25:36 +0530

    more changes

--- stl.cpp
+++ stl.cpp
@@ -1,6 +1,122 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+int main() {
+    set<int> s;
+
+    s.insert(5);
+    s.insert(1);
+    s.insert(3);
+    s.insert(2);
+    s.insert(4);
+    s.insert(5);
+    s.insert(5);
+    s.insert(1);
+
+    for (int i:s) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    s.erase(next(s.begin()));
+
+    for (int i:s) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Size of set: "<<s.size()<<endl;
+    cout<<"Is set empty: "<<s.empty()<<endl;
+    cout<<"Element present or not: "<<s.count(2)<<endl;
+
+    set<int>::iterator it = s.find(3);
+    cout<<"Element found: "<<*it<<endl;
+
+}
+
+
+
+// STL: Priority Queue
+/*
+int main() {
+    // max heap
+    priority_queue<int> max_pq;
+
+    // min heap
+    priority_queue<int, vector<int>, greater<int>> min_pq;
+
+    max_pq.push(11);
+    max_pq.push(47);
+    max_pq.push(7);
+    max_pq.push(107);
+    max_pq.push(25);
+
+    cout<<"Size of max heap: "<<max_pq.size()<<endl;
+    cout<<"Top element of max heap: "<<max_pq.top()<<endl;
+    cout<<"Is max heap empty: "<<max_pq.empty()<<endl;
+
+    int n = max_pq.size();
+    
+    for (int i=0; i<n; i++) {
+        cout<<max_pq.top()<<" ";
+        max_pq.pop();
+    }
+    cout<<endl;
+    
+    cout<<"Is max heap empty: "<<max_pq.empty()<<endl;
+
+    cout<<endl<<endl;
+
+    min_pq.push(14);
+    min_pq.push(10);
+    min_pq.push(7);
+    min_pq.push(19);
+
+    cout<<"Size of min heap: "<<min_pq.size()<<endl;
+    cout<<"Top element of min heap: "<<min_pq.top()<<endl;
+    cout<<"Is min heap empty: "<<min_pq.empty()<<endl;
+
+    int m = min_pq.size();
+
+    for (int i=0; i<m; i++) {
+        cout<<min_pq.top()<<" ";
+        min_pq.pop();
+    }
+    cout<<endl;
+
+    cout<<"Is min heap empty: "<<min_pq.empty()<<endl;
+}
+*/
+
+
+
+// STL: Queue
+/*
+int main() {
+    queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+
+    cout<<"Size of queue: "<<q.size()<<endl;
+    cout<<"Front element of queue: "<<q.front()<<endl;
+    cout<<"Back element of queue: "<<q.back()<<endl;
+    cout<<"Is queue empty: "<<q.empty()<<endl;
+
+    q.pop();
+
+    cout<<"After pop:- "<<endl;
+    cout<<"Size of queue: "<<q.size()<<endl;
+    cout<<"Front element of queue: "<<q.front()<<endl;
+}
+*/
+
+
+// STL: Stack
+/*
 int main() {
     stack<int> s;
     s.push(1);
@@ -11,15 +127,8 @@ int main() {
     cout<<"Size of stack: "<<s.size()<<endl;
     cout<<"Top element of stack: "<<s.top()<<endl;
     cout<<"Is stack empty: "<<s.empty()<<endl;
-
-    for (int i=1; i<s.size(); i++) {
-        cout<<i<<" ";
-    }
-    cout<<endl;
 }   
-
-
-
+*/
 
 
 // STL: List
